477f1cbafa661b1bb86187b59f4db6bd2c93706c
bpsingh1982 <bobbysingh20051@live.com>
2019-01-02 21:30:01 +0530

    clean with recreate files

--- dist/master.js
+++ dist/master.js
@@ -1,9 +1,2 @@
-$('.mymodal').autoModal({
-  "config": {
-    "id": "Example",
-    "footer": false,
-    "size": 'sm'
-  },
-  "title": "Sample Title 2",
-  "body": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur saepe delectus doloremque suscipit velit deleniti, tempore ratione! Corrupti, molestiae neque vero facere quam nihil dolores, nam ipsum recusandae, quo maiores."
-});
+$(".mymodal").autoModal({config:{id:"Example",footer:!1,size:"sm"},title:"Sample Title 2",body:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur saepe delectus doloremque suscipit velit deleniti, tempore ratione! Corrupti, molestiae neque vero facere quam nihil dolores, nam ipsum recusandae, quo maiores."});
+//# sourceMappingURL=master.js.map
