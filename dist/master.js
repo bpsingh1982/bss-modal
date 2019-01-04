9f712c8be04221e43845440c8bada0947484ee27
bpsingh1982 <bobbysingh20051@live.com>
2019-01-04 10:47:34 +0530

    change is running

--- dist/master.js
+++
@@ -1,2 +0,0 @@
-$(".mymodal").autoModal({config:{id:"Example",footer:!1,size:"sm"},title:"Sample Title 2",body:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur saepe delectus doloremque suscipit velit deleniti, tempore ratione! Corrupti, molestiae neque vero facere quam nihil dolores, nam ipsum recusandae, quo maiores."});
-//# sourceMappingURL=master.js.map
